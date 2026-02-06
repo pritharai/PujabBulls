f543a9b52ee0b7c57aeffa305f578c2b654ad1f9
Pritha Rai <pritharai873@gmail.com>
2026-02-06 15:03:18 +0530

    splashscreen removed

--- PunjabBulls/src/App.jsx
+++ PunjabBulls/src/App.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 import Layout from "./components/Layout/Layout";
-import SplashScreen from "./components/SplashScreen";
+import ScrollToTop from "./components/Layout/ScrollToTop";
 
 import Home from "./Pages/Home";
 import Industries from "./Pages/Industries";
@@ -11,42 +10,23 @@ import Products from "./Pages/Products";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import PrivacyPolicy from "./Pages/Privacy";
-import ScrollToTop from "./components/Layout/ScrollToTop";
 
 function App() {
-  const [showSplash, setShowSplash] = useState(() => {
-    return !sessionStorage.getItem("punjabbulls_splash_seen");
-  });
-
-  useEffect(() => {
-    if (!showSplash) return;
-
-    const timer = setTimeout(() => {
-      sessionStorage.setItem("punjabbulls_splash_seen", "true");
-      setShowSplash(false);
-    }, 3500);
-
-    return () => clearTimeout(timer);
-  }, [showSplash]);
-
-  if (showSplash) {
-    return <SplashScreen onFinish={() => setShowSplash(false)} />;
-  }
-
   return (
     <>
-    < ScrollToTop />
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/industries" element={<Industries />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Route>
-    </Routes>
+      <ScrollToTop />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/industries" element={<Industries />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Route>
+      </Routes>
     </>
   );
 }
