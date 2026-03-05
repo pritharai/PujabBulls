5328a87f6fb341bd49513e8da3a570264c05d005
Pritha Rai <138858911+pritharai@users.noreply.github.com>
2026-03-05 09:06:33 +0530

    Update API base URL to use environment variable

--- PunjabBulls/src/api/client.js
+++ PunjabBulls/src/api/client.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://www.punjabbulls.com",
-  "https://punjabbulls.com",
+  baseURL: meta.env.VITE_BASE_URL
 });
 
 // Attach token automatically
