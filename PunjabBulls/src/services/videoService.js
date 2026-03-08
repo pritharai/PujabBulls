bc8dd70af91d0ed040fd7c05125ad063fd5c0a70
Pritha Rai <pritharai873@gmail.com>
2026-03-08 12:21:50 +0530

    fixed routing issues

--- PunjabBulls/src/services/videoService.js
+++ PunjabBulls/src/services/videoService.js
@@ -1,21 +1,21 @@
 import api from "../api/client";
 
 export const uploadVideo = async (data) => {
-  const res = await api.post("/videos", data);
+  const res = await api.post("/api/videos", data);
   return res.data;
 };
 
 export const getVideosByPage = async (slug) => {
-  const res = await api.get(`/videos/${slug}`);
+  const res = await api.get(`/api/videos/${slug}`);
   return res.data.data;
 };
 
 export const getAllVideos = async () => {
-  const res = await api.get("/videos");
+  const res = await api.get("/api/videos");
   return res.data.data;
 };
 
 export const deleteVideo = async (id) => {
-  const res = await api.delete(`/videos/${id}`);
+  const res = await api.delete(`/api/videos/${id}`);
   return res.data;
 };
